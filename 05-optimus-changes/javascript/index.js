a90a774c49b6f1bc5b9d784f66c7b9e7e99392b8
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-08-09 09:45:21 -0500

    feat: update optimusChange function to include minimum coin count in output

--- 05-optimus-changes/javascript/index.js
+++ 05-optimus-changes/javascript/index.js
@@ -3,7 +3,6 @@ Monedas: [1, 3, 4]
 Valor: 6  
 
 Salida:
-Cantidad mínima: 2  
 Monedas usadas: [3, 3]
  
  */
@@ -11,7 +10,25 @@ Monedas usadas: [3, 3]
 function optimusChange(arr, target) {
   let acc = 0;
 
-  for(let i = 0; i < arr.length; i++){
-    //primero validar si la suma asi mismo 
+  //casos a evaluar
+
+  //sumar el primer elemento con el siguiente => target | devolver [ pos1, pos2 ]
+  // caso de que no se puede sumar con siguiente usar el mas adelante, y asi sucesivamente
+  // sino es ninguno establecemos el valor a la siguiente posicion del array y realizamos el mismo proceso
+
+  //recorremos linealmente el array
+  for (let i = 0; i < arr.length; i++) {
+    //recorremos en reversa el array
+    for (let j = arr.length - 1; j >= 0; j--) {
+      acc = arr[i] + arr[j];
+
+      if (acc === target) {
+        return { message: "Objetivo logrado", moneys: [arr[i], arr[j]] };
+      }
+
+      acc = 0;
+    }
   }
 }
+
+console.log(optimusChange([1, 3, 4], 11) ?? "No se encontraron monedas");
